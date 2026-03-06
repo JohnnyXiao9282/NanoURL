7f5aba4dd04fde8238a3af7acde22622902c0f58
Johnny Xiao <yuxiao9282@gmail.com>
2026-03-05 20:59:23 -0800

    basic setup for shortening

---
+++ backend/UrlShortener.cpp
@@ -0,0 +1,18 @@
+#include "UrlShortener.h"
+#include <random>
+#include <string>
+#include <unordered_map>
+
+
+
+std::string UrlShortener::shorten(const std::string& longUrl) {
+   
+}
+
+std::string UrlShortener::expand(const std::string& shortUrl) const {
+
+}
+
+std::string UrlShortener::generateShortUrl(const std::string& longUrl) {
+    
+}
