3ac58b53ac9e02eca6ad35df116f0158be5b639c
Johnny Xiao <yuxiao9282@gmail.com>
2026-03-06 20:54:56 -0800

    update main

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,13 +1,15 @@
 #include "UrlShortener.h"
+#include "UrlShortener.cpp" // Ensure SimpleUrlShortener is visible
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    UrlShortener shortener;
+    UrlShortener* shortener = new SimpleUrlShortener();
     string longUrl = "https://www.example.com";
-    string shortUrl = shortener.shorten(longUrl);
+    string shortUrl = shortener->shorten(longUrl);
     cout << "Shortened: " << shortUrl << endl;
-    cout << "Expanded: " << shortener.expand(shortUrl) << endl;
+    cout << "Expanded: " << shortener->expand(shortUrl) << endl;
+    delete shortener;
     return 0;
 }
