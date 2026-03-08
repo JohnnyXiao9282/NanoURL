3c4a970b243b1aec04bb1ba49786d95bfc41639f
Johnny Xiao <yuxiao9282@gmail.com>
2026-03-07 18:04:51 -0800

    finish urlshortener unit tests

--- backend/test/TestUrlShortener.cpp
+++ backend/test/TestUrlShortener.cpp
@@ -26,21 +26,41 @@ void test_collision_handling() {
 }
 
 void test_empty_url() {
-
+    SimpleUrlShortener shortener;
+    string emptyUrl = "";
+    string shortUrl = shortener.shorten(emptyUrl);
+    assert(shortUrl.length() > 0);
+    string expanded = shortener.expand(shortUrl);
+    assert(expanded == emptyUrl);
+    cout << "test_empty_url passed." << endl;
 }
 
 void test_repeat_shorten() {
-
+    SimpleUrlShortener shortener;
+    string url = "https://www.repeat.com";
+    string short1 = shortener.shorten(url);
+    string short2 = shortener.shorten(url);
+    assert(short1 == short2);
+    string expanded = shortener.expand(short1);
+    assert(expanded == url);
+    cout << "test_repeat_shorten passed." << endl;
 }
 
 void test_invalid_expand() {
-
+    SimpleUrlShortener shortener;
+    string invalidShortUrl = "abcdef";
+    string expanded = shortener.expand(invalidShortUrl);
+    assert(expanded == "");
+    cout << "test_invalid_expand passed." << endl;
 }
 
 
 int main() {
-	test_shorten_and_expand();
-	test_collision_handling();
-	cout << "All tests passed." << endl;
-	return 0;
+    test_shorten_and_expand();
+    test_collision_handling();
+    test_empty_url();
+    test_repeat_shorten();
+    test_invalid_expand();
+    cout << "All tests passed." << endl;
+    return 0;
 }
